820f76c4230fbd3cbec7f5432e079996ed8daa2e
neemajd20 <neemajd20@gmail.com>
2024-08-23 16:12:34 +0300

    app.config.ts,login.component.css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+/**
+ * Configuration settings for the application.
+ */
+const ApplicationConfig = {
+  // Your configuration settings here
 };
+
+
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)];
